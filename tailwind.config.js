e9fc7487e929c2da9997496098a2efd520abc2f9
crea8ivedev <crea8ivedev@gmail.com>
2025-01-02 15:34:06 +0530

    chore: tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2BD17E",
+        "primary-light": "#224957",
+        secondary: "#093545",
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+};
